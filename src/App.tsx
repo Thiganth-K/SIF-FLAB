1530b6265eab2393a332ceb79ed425f3cce4eab4
Thiganth-K <thiganthworkspace02@gmail.com>
2025-09-27 00:11:16 +0530

    added contact section for neha

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import HeroSection from './sections/HeroSection';
 import AboutPage from './sections/AboutPage';
+import ContactSection from './sections/ContactSection';
 
 function App() {
   return (
     <>
       <HeroSection />
       <AboutPage />
+      <ContactSection />
     </>
   );
 }
